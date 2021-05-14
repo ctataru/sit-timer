caf7004478555ee8ee92fdc32651de9aa924c4cc
Clement <ctataru@hotmail.com>
2021-05-15 00:52:05 +0200

    feature: add basic popup and icon

---
+++ popup/popup.js
@@ -0,0 +1,5 @@
+// add event listener to get input value for intervals
+
+// add even listener to get on click for "start" and "i'm back!"
+
+// re-open popup when timer is finished and display "i'm back!" button (should this be in background.js?)
